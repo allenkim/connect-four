e8bca4cb3a57f5b0dcd712b9c527ab5acf6cca89
Allen Kim <allenkim95@gmail.com>
2016-12-03 22:35:48 -0500

    Add tests

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 QUnit.test( "Test numWinningStates", function( assert ) {
+  var totalWinningStates = 4*6 + 3*7 + 24;
   var grid = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
@@ -7,6 +8,26 @@ QUnit.test( "Test numWinningStates", function( assert ) {
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0]
   ];
-  assert.equal(numWinningStates(1,grid), 4*6);
-  assert.equal(numWinningStates(1,grid),3);
+  assert.equal(numWinningStates(1,grid), totalWinningStates);
+  assert.equal(numWinningStates(2,grid), totalWinningStates);
+  grid = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0]
+  ];
+  assert.equal(numWinningStates(1,grid), totalWinningStates);
+  assert.equal(numWinningStates(2,grid), totalWinningStates - 3);
+  grid = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,2,0,0,0]
+  ];
+  assert.equal(numWinningStates(1,grid), totalWinningStates - 7);
+  assert.equal(numWinningStates(2,grid), totalWinningStates);
 });
