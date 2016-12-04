db7908b976a10d69eca6b698a5fbc7ce5199dcdd
Porrith Suong <psuong95@gmail.com>
2016-12-03 22:48:32 -0500

    Reverts the latest commit.

--- tests.js
+++ tests.js
@@ -1,5 +1,4 @@
 QUnit.test( "Test numWinningStates", function( assert ) {
-  var totalWinningStates = 4*6 + 3*7 + 24;
   var grid = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
@@ -8,26 +7,6 @@ QUnit.test( "Test numWinningStates", function( assert ) {
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0]
   ];
-  assert.equal(numWinningStates(1,grid), totalWinningStates);
-  assert.equal(numWinningStates(2,grid), totalWinningStates);
-  grid = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [1,0,0,0,0,0,0]
-  ];
-  assert.equal(numWinningStates(1,grid), totalWinningStates);
-  assert.equal(numWinningStates(2,grid), totalWinningStates - 3);
-  grid = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,2,0,0,0]
-  ];
-  assert.equal(numWinningStates(1,grid), totalWinningStates - 7);
-  assert.equal(numWinningStates(2,grid), totalWinningStates);
+  assert.equal(numWinningStates(1,grid), 4*6);
+  assert.equal(numWinningStates(1,grid),3);
 });
