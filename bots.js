356117ff98f96d9600624c31e1bb994faf90b944
Allen Kim <allenkim95@gmail.com>
2016-12-05 23:41:44 -0500

    Add alphabeta

--- bots.js
+++ bots.js
@@ -112,29 +112,77 @@ bots["Minimax_Bot"] = function(){
   return minimax(copyGrid(grid), minimaxDepth, playerTurn);
 }
 
-var player_options = document.getElementsByClassName("player_options");
-for (var i = 1; i <= 2; i++){
-  for (var bot in bots){
-    if (!bots.hasOwnProperty(bot))
-      continue;
-    var id = bot+'_'+i;
-    var button_div = document.createElement('div');
-    button_div.setAttribute('class','left_align');
-    var option = document.createElement('input');
-    option.setAttribute('type','radio');
-    option.setAttribute('id', id);
-    option.setAttribute('name','options_'+i);
-    var label = document.createElement('label')
-    label.setAttribute('for', id);
-    label.innerHTML = bot;
-    button_div.appendChild(option);
-    button_div.appendChild(label);
-    player_options[i-1].appendChild(button_div);
-    (function(i,id, bot){
-      document.getElementById(id).onclick = function() {
-        players[i] = bot;
-        makeBotMove();
-      };
-    })(i,id, bot);
+// returns best move with heuristic
+function alphabeta(grid, depth, alpha, beta, player){
+  if (depth === 0 || isGridFull(grid) || winningState(1, grid) || winningState(2, grid)) {
+    return heuristic(grid);
   }
+  var bestColumn = 0;
+  for (var col = 0; col  < width; col++){
+    var row = moveRow(col,grid);
+    if (row !== -1){
+      bestColumn = col;
+      break;
+    }
+  }
+  // Max's turn
+  if (player === 1) {
+    var bestValue = -Infinity;
+    for (var col = 0; col < width; col++) {
+      // Stores the row position if played at the column
+      var row = moveRow(col,grid);
+      // if it's a valid move
+      if (row !== -1) {
+        var copiedGrid = copyGrid(grid);
+        // Update the coordinates to have the player
+        copiedGrid[row][col] = player;
+        var currentValue = alphabeta(copiedGrid, depth - 1, alpha, beta, 2);
+        if (currentValue > bestValue) {
+          bestValue = currentValue;
+          bestColumn = col;
+        }
+        alpha = Math.max(alpha, bestValue);
+        if (beta <= alpha)
+          break;
+      }
+    }
+    if (depth === alphabetaDepth)
+      return bestColumn;
+
+    return bestValue;
+  }
+  // Min's turn
+  else {
+    var bestValue = Infinity;
+    for (var col = 0; col < width; col++) {
+      // Stores the row position if played at the column
+      var row = moveRow(col,grid);
+      // if it's a valid move
+      if (row !== -1) {
+        var copiedGrid = copyGrid(grid);
+        // Update the coordinates to have the player
+        copiedGrid[row][col] = player;
+        var currentValue = alphabeta(copiedGrid, depth - 1, alpha, beta, 1);
+        if (currentValue < bestValue) {
+          bestValue = currentValue;
+          bestColumn = col;
+        }
+        beta = Math.min(beta, bestValue);
+        if (beta <= alpha)
+          break;
+      }
+    }
+    if (depth === alphabetaDepth)
+      return bestColumn;
+
+    return bestValue;
+  }
+}
+
+// Deepest check for our state space graph
+var alphabetaDepth = 6;
+
+// Player 1 is max, player 2 is min
+bots["AlphaBeta_Bot"] = function(){
+  return alphabeta(copyGrid(grid), alphabetaDepth, -Infinity, Infinity, playerTurn);
 }
