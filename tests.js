003ef3a2f60815d3593b81842ae9567401110b4d
Allen Kim <allenkim95@gmail.com>
2016-12-06 15:12:31 -0500

    Add failing tests

--- tests.js
+++ tests.js
@@ -195,4 +195,16 @@ QUnit.test( "Test winningState", function( assert ) {
   // Test #22
   assert.equal(winningState(2,grid), true);
 
+  grid = [
+    [0,0,0,0,0,0,2],
+    [1,1,2,0,1,0,2],
+    [2,2,1,2,1,0,2],
+    [2,1,1,2,2,2,1],
+    [2,2,1,1,1,2,1],
+    [1,1,0,1,2,2,1]
+  ];
+  // Test #21
+  assert.equal(winningState(1,grid), false);
+  // Test #22
+  assert.equal(winningState(2,grid), true);
 });
